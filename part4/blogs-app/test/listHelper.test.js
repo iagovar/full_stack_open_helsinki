d80ffabed18697a91f77b5ae4f5a7675449037bc
iagovar <iagovar@outlook.com>
2024-03-30 16:52:28 +0100

    Finixed exercise 4.7

--- part4/blogs-app/test/listHelper.test.js
+++ part4/blogs-app/test/listHelper.test.js
@@ -150,14 +150,179 @@ describe("Favorite Blog Reducer", () => {
         ).toThrowError("Argument must be an array");
     });
 
-    test("List of only one blog returns the likes of that one", () => {
+    test("List of only one blog returns that very blog as favorite", () => {
         const result = listHelper.favoriteBlog(blogs.slice(0, 1));
         expect(result).toEqual([blogs[0]]);
     });
     
-    test("Favorite blogs of thest blogs must be 5a422b3a1b54a676234d17f9 and 5a422bc61b54a676234d17fc", () => {
+    test("Favorite blogs of test blogs must be 5a422b3a1b54a676234d17f9 and 5a422bc61b54a676234d17fc", () => {
         const result = listHelper.favoriteBlog(blogs);
         const favortiesArray = [blogs[2], blogs[5]];
         expect(result).toEqual(favortiesArray);
     });
+});
+
+describe("Prolific author Reducer", () => {
+    const blogs = [
+        {
+            _id: "5a422a851b54a676234d17f7",
+            title: "React patterns",
+            author: "Michael Chan",
+            url: "https://reactpatterns.com/",
+            likes: 7,
+            __v: 0
+        },
+        {
+            _id: "5a422aa71b54a676234d17f8",
+            title: "Go To Statement Considered Harmful",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: "5a422b3a1b54a676234d17f9",
+            title: "Canonical string reduction",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+            likes: 12,
+            __v: 0
+        },
+        {
+            _id: "5a422b891b54a676234d17fa",
+            title: "First class tests",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+            likes: 10,
+            __v: 0
+        },
+        {
+            _id: "5a422ba71b54a676234d17fb",
+            title: "TDD harms architecture",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+            likes: 0,
+            __v: 0
+        },
+        {
+            _id: "5a422bc61b54a676234d17fc",
+            title: "Type wars",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+            likes: 12,
+            __v: 0
+        }  
+    ];
+
+    test("Empty list throws error", () => {
+        expect(
+            () => listHelper.mostBlogs([])
+        ).toThrowError("Array must not be empty");        
+    });
+
+    test("Should throw error if argument is not an array", () => {
+        expect(
+            () => listHelper.mostBlogs("test")
+        ).toThrowError("Argument must be an array");
+    });
+
+    test("List of only one blog returns that very author", () => {
+        const result = listHelper.mostBlogs(blogs.slice(0, 1));
+        expect(result).toEqual({
+            author: "Michael Chan",
+            blogs: 1
+        });
+    });
+
+    test("Most prolific author of test blogs is Robert C. Martin with 3 blogs", () => {
+        const result = listHelper.mostBlogs(blogs);
+        expect(result).toEqual({
+            author: "Robert C. Martin",
+            blogs: 3
+        });
+    });
+
+});
+
+describe("Favorite author Reducer", () => {
+    const blogs = [
+        {
+            _id: "5a422a851b54a676234d17f7",
+            title: "React patterns",
+            author: "Michael Chan",
+            url: "https://reactpatterns.com/",
+            likes: 7,
+            __v: 0
+        },
+        {
+            _id: "5a422aa71b54a676234d17f8",
+            title: "Go To Statement Considered Harmful",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: "5a422b3a1b54a676234d17f9",
+            title: "Canonical string reduction",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+            likes: 12,
+            __v: 0
+        },
+        {
+            _id: "5a422b891b54a676234d17fa",
+            title: "First class tests",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+            likes: 10,
+            __v: 0
+        },
+        {
+            _id: "5a422ba71b54a676234d17fb",
+            title: "TDD harms architecture",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+            likes: 0,
+            __v: 0
+        },
+        {
+            _id: "5a422bc61b54a676234d17fc",
+            title: "Type wars",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+            likes: 12,
+            __v: 0
+        }  
+    ];
+
+    test("Empty list throws error", () => {
+        expect(
+            () => listHelper.mostLikes([])
+        ).toThrowError("Array must not be empty");        
+    });
+
+    test("Should throw error if argument is not an array", () => {
+        expect(
+            () => listHelper.mostLikes("test")
+        ).toThrowError("Argument must be an array");
+    });
+
+    test("List of only one blog returns that very author", () => {
+        const result = listHelper.mostLikes(blogs.slice(0, 1));
+        expect(result).toEqual({
+            author: "Michael Chan",
+            likes: 7
+        });
+    });
+
+    test("Favorite author of test blogs is Robert C. Martin with 22 likes", () => {
+        const result = listHelper.mostLikes(blogs);
+        expect(result).toEqual({
+            author: "Robert C. Martin",
+            likes: 22
+        });
+    });
+
+
 });
--- part4/blogs-app/utils/list_helper.js
+++ part4/blogs-app/utils/list_helper.js
@@ -43,12 +43,90 @@ function favoriteBlog(ArrayOfBlogObjects) {
 }
 
 
+function mostBlogs(ArrayOfBlogObjects) {
+    // Check if it's an array
+    if (Array.isArray(ArrayOfBlogObjects) === false) {
+        throw new Error("Argument must be an array");
+    }
+
+    // Check if the array is empty
+    if (ArrayOfBlogObjects.length === 0) {
+        throw new Error("Array must not be empty");
+    }
+
+    // Make a list of authors
+    const listOfAuthors = ArrayOfBlogObjects.map(blog => blog.author);
+    const authorCount = {};
+
+    // Count the times an author appears in the list
+    for (let author of listOfAuthors) {
+        if (author in authorCount) {
+            authorCount[author] += 1;
+        } else {
+            authorCount[author] = 1;
+        }
+    }
+
+    // Find the author with the highest count
+    const max = Math.max(...Object.values(authorCount));
+
+    // Return the name and times of the first author with the highest count
+    for (let author in authorCount) {
+        if (authorCount[author] === max) {
+            return {
+                author: author,
+                blogs: authorCount[author]
+            };
+        }
+    }
+}
+
+function mostLikes(ArrayOfBlogObjects) {
+    // Check if it's an array
+    if (Array.isArray(ArrayOfBlogObjects) === false) {
+        throw new Error("Argument must be an array");
+    }
+
+    // Check if the array is empty
+    if (ArrayOfBlogObjects.length === 0) {
+        throw new Error("Array must not be empty");
+    }
+
+    // Make a set of authors
+    const setOfAuthors = new Set(ArrayOfBlogObjects.map(blog => blog.author));
+
+    // Build a an object with the authors as keys and 0 as values (the likes)
+    let authorCount = {};
+    for (let author of setOfAuthors) {
+        authorCount[author] = 0;
+    }
+
+    // Count the likes for each author
+    for (let blog of ArrayOfBlogObjects) {
+        authorCount[blog.author] += blog.likes;
+    }
+
+    // Find the author with the highest count
+    const max = Math.max(...Object.values(authorCount));
+
+    // Return the name and times of the first author with the highest count
+    for (let author in authorCount) {
+        if (authorCount[author] === max) {
+            return {
+                author: author,
+                likes: authorCount[author]
+            };
+        }
+    }
+}
     
 
   
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 };
 
